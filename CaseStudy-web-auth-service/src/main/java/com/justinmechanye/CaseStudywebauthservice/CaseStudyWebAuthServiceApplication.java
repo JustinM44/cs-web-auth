eb2210d00fdeb061604da7c1aad73a0aa35a7933
JustinM44 <justin.mechanye@outlook.com>
2021-01-09 09:31:08 -0600

    added base spring boot api for auth

---
+++ CaseStudy-web-auth-service/src/main/java/com/justinmechanye/CaseStudywebauthservice/CaseStudyWebAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.justinmechanye.CaseStudywebauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseStudyWebAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseStudyWebAuthServiceApplication.class, args);
+	}
+
+}
